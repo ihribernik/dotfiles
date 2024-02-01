bfbaf3286c6e7e0654745bcf7bb5fc289138f47d
Ivan Hribernik <cihribernik@gmail.com>
2024-02-01 16:55:03 -0300

    added dotfiles and config install.conf.yaml

---
+++ dotfiles/bash/.bash_aliases
@@ -0,0 +1,16 @@
+if [ -x "$(command -v batcat)" ];then
+    alias bat="batcat"
+    export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
+fi
+
+if [ -x "$(command -v minikube)" ];then
+    export INGRESS_HOST=$(minikube ip)
+fi
+
+if [ -x "$(command -v vim)" ];then
+    export EDITOR=vim
+fi
+
+alias open='xdg-open'
+
+alias pcrun="pre-commit run --all-files"
---
+++ dotfiles/powershell/user_profile.ps1
@@ -0,0 +1,40 @@
+# Prompt
+Import-Module posh-git
+$personalFolder = $ExecutionContext.InvokeCommand.ExpandString('$home\powershell')
+$themesFolder = $ExecutionContext.InvokeCommand.ExpandString('$home\AppData\Local\Programs\oh-my-posh\themes')
+$themeFile = "gruvbox.omp.json"
+$theme = Join-Path $themesFolder $themeFile
+oh-my-posh init pwsh --config $theme | Invoke-Expression
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# Alias
+Set-Alias vim nvim
+
+# Utilities
+function which($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+      Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+
+function Touch-File() {
+    $fileName = $args[0]
+    # Comprobar si el archivo existe
+    if (-not(Test-Path $fileName)) {
+        # Si no existe, crealo
+        New-Item -ItemType File -Name $fileName
+    }
+    else {
+        #Si existe, actualiza el timestamp
+        (Get-ChildItem $fileName).LastWriteTime = Get-Date
+    }
+}
+
+### Creando un alias para touch
+
+# Checar si el alias touch existe
+if (-not(Test-Path -Path Alias:Touch)) {
+    New-Alias -Name Touch Touch-File -Force
+}
