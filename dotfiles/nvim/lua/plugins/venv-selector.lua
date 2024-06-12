6403d0a2be9c5536ade13846a3ae2d612bf54fa1
Ivan Hribernik <cihribernik@gmail.com>
2024-06-12 16:37:08 -0300

    added better venv selector

--- dotfiles/nvim/lua/plugins/venv-selector.lua
+++ dotfiles/nvim/lua/plugins/venv-selector.lua
@@ -1,6 +1,8 @@
 return {
   "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
   cmd = "VenvSelect",
+  -- branch = "regexp", -- This is the regexp branch, use this for the new version
   opts = function(_, opts)
     if require("lazyvim.util").has("nvim-dap-python") then
       opts.dap_enabled = true
