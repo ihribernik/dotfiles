d820178678646a522c581fab88f5af7883a892ff
Ivan Hribernik <cihribernik@gmail.com>
2024-07-20 21:15:31 -0300

    backup 2072024

--- dotfiles/nvim/lua/plugins/venv-selector.lua
+++ dotfiles/nvim/lua/plugins/venv-selector.lua
@@ -1,20 +1,18 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  branch = "regexp", -- Use this branch for the new version
   cmd = "VenvSelect",
-  -- branch = "regexp", -- This is the regexp branch, use this for the new version
-  opts = function(_, opts)
-    if require("lazyvim.util").has("nvim-dap-python") then
-      opts.dap_enabled = true
-    end
-    return vim.tbl_deep_extend("force", opts, {
-      name = {
-        "venv",
-        ".venv",
-        "env",
-        ".env",
-      },
-    })
+  enabled = function()
+    return LazyVim.has("telescope.nvim")
   end,
-  keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+  opts = {
+    settings = {
+      options = {
+        notify_user_on_venv_activation = true,
+      },
+    },
+  },
+  --  Call config for python files and load the cached venv automatically
+  ft = "python",
+  keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
 }
