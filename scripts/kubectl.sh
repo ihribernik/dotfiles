af7130a5eb74b3cb0f67b05b5d8986b3901d8c70
Ivan Hribernik <cihribernik@gmail.com>
2024-10-31 11:58:21 -0300

    added kubectl and minikube

---
+++ scripts/kubectl.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
