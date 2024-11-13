df2be61b6f40f226bbae335ea6527b033ffffa50
Ivan Hribernik <cihribernik@gmail.com>
2024-11-13 16:48:18 -0300

    change  neotree location

--- dotfiles/nvim/lua/plugins/neotree.lua
+++ dotfiles/nvim/lua/plugins/neotree.lua
@@ -1,3 +1,9 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right", -- Cambia la posición de "left" a "right"
+      width = 30, -- Opcional: ajusta el ancho si es necesario
+    },
+  },
 }
