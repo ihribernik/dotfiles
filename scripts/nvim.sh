20dd7d5d363d2cee2e1c99799ab9c06f8bddbe97
Ivan Hribernik <cihribernik@gmail.com>
2024-12-01 15:46:25 -0300

    added nvim build

---
+++ scripts/nvim.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+sudo apt-get install ninja-build gettext cmake unzip curl build-essential
+git clone https://github.com/neovim/neovim $HOME/dev/personal/neovim
+cd $HOME/dev/personal/neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=Release
+sudo make install
