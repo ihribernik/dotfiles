1dba57e29997da7e52f73627d39d5f81b26f5545
Ivan Hribernik <cihribernik@gmail.com>
2025-01-01 19:40:15 -0300

    added sublime sync

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo apt-get install ninja-build gettext cmake unzip curl build-essential
+sudo apt-get install ninja-build gettext cmake unzip curl build-essential -y
 git clone https://github.com/neovim/neovim $HOME/dev/personal/neovim
 cd $HOME/dev/personal/neovim
 git checkout stable
