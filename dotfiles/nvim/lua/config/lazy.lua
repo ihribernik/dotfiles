948f628070a417afd6208607ae6319d4224aa5b6
Ivan Hribernik <cihribernik@gmail.com>
2025-01-08 16:55:48 -0300

    added updates

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -27,12 +27,12 @@ require("lazy").setup({
       },
     },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "plugins" },
   },
   defaults = {
--- dotfiles/nvim/lua/plugins/notify.lua
+++
@@ -1,6 +0,0 @@
--- prevent error on transparent background_colour
-require("notify").setup({
-  background_colour = "#252835",
-})
-
-return {}
