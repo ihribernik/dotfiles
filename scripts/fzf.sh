2088f5ff3986e090dcda962a3ba964a9d0c7b750
Ivan Hribernik <cihribernik@gmail.com>
2025-01-10 16:54:49 -0300

    added go and rust installer

---
+++ scripts/fzf.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+git clone git@github.com:junegunn/fzf.git $HOME/dev/personal/fzf
+cd $HOME/dev/personal/fzf
+sudo make install
---
+++ scripts/golang.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
