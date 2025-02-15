947b9b5ba7f9a43ae6c8c417bd99d77171c293d5
Ivan Hribernik <cihribernik@gmail.com>
2025-02-15 00:22:25 -0300

    added better golang install

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
 wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
+rm go1.23.4.linux-amd64.tar.gz
