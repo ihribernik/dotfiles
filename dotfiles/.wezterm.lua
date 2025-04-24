9a5096385826a04f5e2e712e313b670137dd3547
Ivan Hribernik <cihribernik@gmail.com>
2025-04-23 22:53:01 -0300

    added bottom bar in wezterm

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -7,6 +7,7 @@ config.default_prog = { "bash" }
 config.audible_bell = "Disabled"
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 config.keys = { { key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) } }
@@ -15,6 +16,6 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "pwsh" }
 end
 
-config.font = wezterm.font("JetBrainsMono Nerd Font", { italic = false })
+config.font = wezterm.font("JetBrainsMono Nerd Font", { italic = false})
 
 return config
