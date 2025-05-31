7c7123cbc5f13af645e2ec91895f1a60e746d450
Ivan Hribernik <cihribernik@gmail.com>
2025-05-31 16:18:39 -0300

    adde subl themes

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -25,7 +25,7 @@ config.default_prog = { "bash" }
 
 config.audible_bell = "Disabled"
 config.font_size = 12
-config.color_scheme = get_color_scheme()
+-- config.color_scheme = get_color_scheme()
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
