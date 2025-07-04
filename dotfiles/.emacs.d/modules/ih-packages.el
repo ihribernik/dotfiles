806a6e7c6fde02758ece01846696a5462a7f1a1b
Ivan Hribernik <cihribernik@gmail.com>
2025-07-04 18:58:08 -0300

    feat: backup

--- dotfiles/.emacs.d/modules/ih-packages.el
+++ dotfiles/.emacs.d/modules/ih-packages.el
@@ -14,10 +14,12 @@
   :ensure t
   :commands lsp-ui-mode)
 
-(projectile-mode +1)
-
-;; Recommended keymap prefix on Windows/Linux
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode +1)
+  ;; Recommended keymap prefix on Windows/Linux
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 (use-package flycheck
   :ensure t
