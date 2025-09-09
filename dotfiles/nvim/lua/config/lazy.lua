88c6ce0cd8f9dd0051a9578f76554a36acdd7f09
ivan hribernik <cihribernik@gmail.com>
2025-09-08 23:08:53 -0300

    fix: change the loop.fs_stat for uv.fs_stat and remove themes

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -1,6 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
+-- in previous versions uses vim.loop.fs_stat
+if not vim.uv.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
   print("cloning lazyvim repo...")
