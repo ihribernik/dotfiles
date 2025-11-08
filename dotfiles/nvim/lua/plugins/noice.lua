ad95182752c175c4625fc657bc9c6b266d0c8983
Ivan Hribernik <cihribernik@gmail.com>
2025-11-08 00:36:02 -0300

    chore: remove extras that didnt use

--- dotfiles/nvim/lua/plugins/noice.lua
+++ dotfiles/nvim/lua/plugins/noice.lua
@@ -1,10 +1,14 @@
-return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    messages = { enabled = false },
-    notify = { enabled = false },
-    popupmenu = { enabled = false },
-    cmdline = { enabled = false },
-  },
-}
+if true then
+  return {}
+end
+
+-- return {
+--   "folke/noice.nvim",
+--   event = "VeryLazy",
+--   opts = {
+--     messages = { enabled = false },
+--     notify = { enabled = false },
+--     popupmenu = { enabled = false },
+--     cmdline = { enabled = false },
+--   },
+-- }
