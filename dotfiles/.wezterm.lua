58a28807681813ec8c59e4b06a8345d61ac00938
Ivan Hribernik <cihribernik@gmail.com>
2026-04-26 19:50:10 -0300

    refactor: remove background color

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -25,9 +25,6 @@ config.audible_bell = "Disabled"
 config.font_size = 12
 -- config.color_scheme = get_color_scheme()
 config.window_background_opacity = 0.9
-config.colors = {
-	background = "#031219",
-}
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
@@ -39,7 +36,5 @@ config.keys = {
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "pwsh" }
 end
-
--- config.font = wezterm.font("JetBrainsMono Nerd Font", { italic = false })
 
 return config
