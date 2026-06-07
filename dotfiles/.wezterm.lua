48478f80eeb40567302c3f7a9560895f90fb2497
Ivan Hribernik <cihribernik@gmail.com>
2026-06-06 22:38:18 -0300

    fix some tweaks

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -8,10 +8,11 @@ config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 14
 config.window_background_opacity = 0.9
 
-config.color_scheme = 'iTerm2 Default'
+config.color_scheme = "iTerm2 Default"
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+-- config.default_domain = 'WSL:Ubuntu'
 
 config.keys = {
 	{ key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
